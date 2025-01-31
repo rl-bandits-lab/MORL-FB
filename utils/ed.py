cb0dfbe985d374a108849755acc326af3ee09e84
intutu <st993201@gmail.com>
2025-01-31 12:58:07 +0800

    fix: fix the variable undefined problem

--- utils/ed.py
+++ utils/ed.py
@@ -3,15 +3,15 @@ import numpy as np
 
 
 
-def episodic_domination(fb, pd, pref):
+def episodic_domination(base, others, pref):
     cnt = 0
-    for (fb_, pd_, pref_) in zip(fb, pd, pref):
-        dot_fb = np.dot(fb_, pref_)
-        dot_pd = np.dot(pd_, pref_)
-        if dot_pd >= dot_fb:
+    for (base_, others_, pref_) in zip(base, others, pref):
+        dot_base = np.dot(base_, pref_)
+        dot_others = np.dot(others_, pref_)
+        if dot_others >= dot_base:
             cnt += 1
 
-    return cnt / fb.shape[0]
+    return cnt / base.shape[0]
 
 if __name__ == '__main__':
 
@@ -23,14 +23,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    fb_data = np.load(args.fb)
-    pd_data = np.load(args.pd)
+    base_data = np.load(args.base)
+    others_data = np.load(args.others)
     pref = np.load(args.pref)
 
     eds = []
 
-    for i in range(fb_data.shape[1]):
-        ed = episodic_domination(fb_data[:, i, :], pd_data[:, i, :], pref)
+    for i in range(base_data.shape[1]):
+        ed = episodic_domination(base_data[:, i, :], others_data[:, i, :], pref)
         eds.append(ed)
 
     print(f'Episodic domination: {np.mean(eds)} , {np.std(eds)}')
