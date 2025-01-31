273e84124b3e93694f96b57b357078e063e253c5
intutu <st993201@gmail.com>
2025-01-31 18:36:19 +0800

    style: remove some link inside function
    BREAKING CHANGE:

--- continuous/utils.py
+++ continuous/utils.py
@@ -72,8 +72,6 @@ class TanhTransform(pyd.transforms.Transform):
         return self.atanh(y)
 
     def log_abs_det_jacobian(self, x, y) -> torch.Tensor:
-        # We use a formula that is more numerically stable, see details in the following link
-        # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
         return 2. * (math.log(2.) - x - F.softplus(-2. * x))
 
 
